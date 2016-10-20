358d04639712cac01b254342fd038bbf7d892724
Julian <juliantrodom@gmail.com>
2016-10-20 17:36:11 +0200

    Retoque y arreglo de la clase AddProduct, añadida la traduccion a ingles

--- app/src/main/java/com/julian/manageproduct/AddProduct_Activity.java
+++ app/src/main/java/com/julian/manageproduct/AddProduct_Activity.java
@@ -1,13 +1,73 @@
 package com.julian.manageproduct;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AddProduct_Activity extends AppCompatActivity {
+
+    //array adpter for the spinner
+    //ArrayAdapter<CharSequence> arrayAdapter=ArrayAdapter.createFromResource(this,R.array.listUnit,android.R.layout.simple_spinner_item);
+
+    private Spinner spOptions;
+    private Button btnSave;
+    private EditText edtName,edtDescription,edtDosage,edtBrand,edtPrice,edtStock;
+    private String imgUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product_);
+
+        // metodo para enlazar las variables con los componentes que tenemos en el XML
+        inicialize();
+    }
+
+    private void inicialize() {
+        spOptions=(Spinner)findViewById(R.id.spnUnitDosage);
+        btnSave=(Button)findViewById(R.id.btnSave);
+        edtName=(EditText)findViewById(R.id.edtName);
+        edtDescription=(EditText)findViewById(R.id.edtDescrip);
+        edtDosage=(EditText)findViewById(R.id.edtDosage);
+        edtBrand=(EditText)findViewById(R.id.edtBrand);
+        edtPrice=(EditText)findViewById(R.id.edtPrice);
+        edtStock=(EditText)findViewById(R.id.edtStock);
+
+
+        //*----------------------------------*//
+      //  arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+      //  spOptions.setAdapter(arrayAdapter);
+    }
+
+    //cargar las diferentes opciones cuando se crea la activity
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+
+    public void clickImg(View view) {
+        /*Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("file/*");
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(intent,1);
+        else
+
+            Toast.makeText(this, "No hay aplicación para manejar ficheros", Toast.LENGTH_SHORT).show();*/
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+
+    }
+
+    public void clickbtn(View view) {
     }
 }
--- app/src/main/java/com/julian/manageproduct/ListProduct_Activity.java
+++ app/src/main/java/com/julian/manageproduct/ListProduct_Activity.java
@@ -1,10 +1,14 @@
 package com.julian.manageproduct;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.julian.manageproduct.model.Product;
 
@@ -12,6 +16,9 @@ import com.julian.manageproduct.model.Product;
 //nuestra interfaz se añadira un elemento llamado list view
 
 public class ListProduct_Activity extends ListActivity {
+
+    //si hay un fallo o no se agrega se queda en  si la agregacion va correctamente toma valor de 1
+    private final static int resultadoAdd=0;
 
     //region explicacion
 
@@ -23,12 +30,17 @@ public class ListProduct_Activity extends ListActivity {
 
     //endregion
 
+    //variables
+   private Button btnAdd;
+
    private ArrayAdapter<Product> adapter;//creacion de un array adapter para el caso 1
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_product);
+
+        btnAdd=(Button)findViewById(R.id.btnAdd);
 
         //CASO 1: Adapter no personalizado      toma el adapter no personalizado de android
         adapter= new ArrayAdapter<Product>(this, android.R.layout.simple_list_item_1,((Product_Aplication) getApplication()).getProducts());
@@ -41,4 +53,11 @@ public class ListProduct_Activity extends ListActivity {
     }
 
 
+    public void clickAdd(View view) {
+
+        Intent i = new Intent(this, AddProduct_Activity.class);
+        // Iniciamos la segunda actividad, y le indicamos que la iniciamos
+        // para rellenar el nombre:
+        startActivity(i);
+    }
 }
